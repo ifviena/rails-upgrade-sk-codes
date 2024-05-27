92817b34a9e61cb173b203859547ef25de221dd4
Ian Mher Viena <35288834+ifviena@users.noreply.github.com>
2024-05-28 03:28:08 +0800

    Rails 5.1 Ruby 2.5

---
+++ test/system/skeletons_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class SkeletonsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit skeletons_url
+  #
+  #   assert_selector "h1", text: "Skeleton"
+  # end
+end
